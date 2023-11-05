c7a1396bf7047b35dc20503ede7c0924014fc686
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-11-05 20:12:08 +0200

    Added new message coresponding to the counter numbers. It incorporates a switch case

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -17,9 +17,39 @@ export default function Counter(props) {
     )
   }
 
+  let message = null;
+
+  switch (count) {
+    case 1:
+      message = 'First blood'
+      break;
+      case 2:
+      message = 'Double kill'
+      break;
+      case 3:
+      message = 'Triple kill'
+      break;
+      case 4:
+      message = 'Multi kill'
+      break;
+      case 5:
+      message = 'Unstoppable'
+      break;
+      case 6:
+      message = 'God like'
+      break;  
+    default:
+      message = 'Monster kill'
+      break;
+  }
+
   return (
     <div>
       <h3>Counter</h3>
+
+      {count == 0 && <p>Please start incrementing</p>}
+
+      <h4>{message}</h4>
 
       <p>Count: {count}</p>
 
