9b33a2eb18318321a9d2a1a2d5a8d52f9e656aa8
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-11-05 18:46:08 +0200

    Initial commit with the basic structure of the app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import MovieList from "./components/MovieList";
+import movies from "./assets/movies"; // gives me ready the list with movies from the asset
+import "./App.css";
+import Timer from "./components/Timer";
+
+
+function App() {
+  return (
+    <div>
+      <h1>My first dynamic react application</h1>
+
+      <Timer startTime={5}/>
+      <Timer startTime={6}/>
+      <Timer startTime={7}/>
+
+      <MovieList movies={movies} headingText="The Movie List"/>
+    </div>
+  );
+}
+
+export default App;
